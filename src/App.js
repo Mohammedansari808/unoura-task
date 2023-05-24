f64e2d050d421363559e42bec58f720b52c652f2
Mohammedansari808 <mohammedansari808@gmail.com>
2023-05-24 17:32:47 +0530

    completed task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const data = [
+
+    { district: "Ariyalur", unions: ["Jayankondam", "Ariyalur"] },
+
+    { district: "Chengalpattu", unions: ["Tambaram", "Chengalpattu"] },
+
+    { district: "Chennai", unions: ["Tondiarpet", "Ambattur", "Chennai"] },
+
+    { district: "Coimbatore", unions: ["Peelamedu", "Gandhipuram", "Coimbatore"] },
+
+    { district: "Cuddalore", unions: ["Neyveli", "Cuddalore"] },
+
+  ]
+
+  const [value, setValue] = useState()
+  const [show, setShow] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ marginTop: "150px" }}>
+      <select style={{ marginRight: "1em" }} onChange={(e) => { setValue(e.target.value); setShow(true) }}>
+        {
+          data.map((ele, ind) => {
+            return (
+              <option value={ind}>{ele.district}</option>
+            )
+          })
+        }
+      </select>
+
+      {show ? (
+        <select>
+          {
+            data[value]?.unions.map((ele) => {
+              return (
+                <option>{ele}</option>
+              )
+            })
+          }
+        </select>
+      ) : null}
     </div>
+
   );
+
 }
 
 export default App;
